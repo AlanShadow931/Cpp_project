da2d476c0c9e562e3700ec7610bc1847a03df8d2
Asomya <100421302+AlanShadow931@users.noreply.github.com>
2025-06-03 17:04:22 +0800

    Delete

--- Baisc_Data_Processing/cmake-build-debug/CMakeFiles/rebuild_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /cygdrive/c/Users/AlanS/CLionProjects/Baisc_Data_Processing/cmake-build-debug
-/cygdrive/c/Users/AlanS/AppData/Local/JetBrains/CLion2025.1/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
--- Cpp改寫ClassCar/cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /cygdrive/c/Users/AlanS/CLionProjects/untitled1/cmake-build-debug
-/cygdrive/c/Users/AlanS/AppData/Local/JetBrains/CLion2025.1/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
