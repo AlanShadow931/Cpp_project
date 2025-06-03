c52130d69695587752f9cd97f52831df2c0a773b
Asomya <100421302+AlanShadow931@users.noreply.github.com>
2025-06-03 22:04:10 +0800

    Upload

---
+++ untitled4/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+    string fileName,input,line;
+    ifstream readfile;
+    ofstream output;
+    cout << "請輸入檔名:";
+    cin >> fileName;
+    output.open(fileName,ios::app);
+    if (output.fail()) {
+        cout << "檔案發生錯誤" << endl;
+        exit(1);
+    }
+    cout << "請輸入內容(欲退出程序請輸入exit):" << endl;
+    while(1) {
+        cin >> input;
+        if(input == "exit")
+            break;
+        output << input << endl;
+    }
+    output.close();
+    readfile.open(fileName,ios::in);
+    if (output.fail()) {
+        cout << "檔案發生錯誤" << endl;
+        exit(1);
+    }
+    cout << "寫入完成，檔案" << fileName << "的內容如下:" << endl;
+    while (getline(readfile,line))
+        cout << line << endl;
+    readfile.close();
+    return 0;
+}
