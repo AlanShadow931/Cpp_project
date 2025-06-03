f042c24ad5ad4d443fc50d1038b0e0a6ec10d82e
EasonShin <eason966604@gmail.com>
2025-06-03 09:05:44 +0800

    Update main.cpp

--- Cpp改寫ClassCar/main.cpp
+++ Cpp改寫ClassCar/main.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <string>
+
 using namespace std;
 class car {
 public:
@@ -36,7 +35,7 @@ car::car(string carname="自小客",int carwheel=4,int carperson=5,string careng
     max_mileage=fuel_capacity*10;
     total_mileage=0;
 }
-truck::truck(string truckname="卡車",int truckwheel=6,int truckperson=3,int trucklength=10,string truckengine="MAX1000-ER")
+truck::truck(string truckname="卡車",int truckwheel=6,int truckperson=3,int trucklength=10,string truckengine="MAX1000-ER"
 : car(truckname,truckwheel,truckperson,truckengine),length(trucklength){
     cout<<"truck建構函式執行中..."<<endl;
 };
