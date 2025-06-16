d7a438f807d2a284d9a7c8755fde5c24ef545638
Asomya <100421302+AlanShadow931@users.noreply.github.com>
2025-06-16 23:29:07 +0800

    Update

--- 改寫ch13_06/main.cpp
+++ 改寫ch13_06/main.cpp
@@ -23,7 +23,7 @@ void car::SetEngine(char* SetEngine)
 // 輸出引擎型號
 void car::CheckEngine()
 {
-    printf("ま篮腹%s\n",engine);
+    printf("引擎型號%s\n",engine);
 }
 
 int main(void)
@@ -31,27 +31,27 @@ int main(void)
     car bus,truck,taxi; // 宣告三種車輛
 
     // 設定公車屬性
-    strcpy(bus.name,"そó");
+    strcpy(bus.name,"公車");
     bus.wheel=6;
     bus.person=40;
     bus.SetEngine("M9000-24");
 
     // 設定卡車屬性
-    strcpy(truck.name,"ó");
+    strcpy(truck.name,"卡車");
     truck.wheel=8;
     truck.person=3;
     truck.SetEngine("TM9000-B6");
 
     // 設定計程車屬性
-    strcpy(taxi.name,"璸祘ó");
+    strcpy(taxi.name,"計程車");
     taxi.wheel=4;
     taxi.person=5;
     taxi.SetEngine("T1600-AC");
 
     // 輸出各車輛資訊及引擎型號
-    printf("%sΤ%d近,更%d ",bus.name,bus.wheel,bus.person);bus.CheckEngine();
-    printf("%sΤ%d近,更%d ",truck.name,truck.wheel,truck.person);truck.CheckEngine();
-    printf("%sΤ%d近,更%d ",taxi.name,taxi.wheel,taxi.person);taxi.CheckEngine();
+    printf("%s有%d個輪子,可載%d人 ",bus.name,bus.wheel,bus.person);bus.CheckEngine();
+    printf("%s有%d個輪子,可載%d人 ",truck.name,truck.wheel,truck.person);truck.CheckEngine();
+    printf("%s有%d個輪子,可載%d人 ",taxi.name,taxi.wheel,taxi.person);taxi.CheckEngine();
 
     return 0;
 }
